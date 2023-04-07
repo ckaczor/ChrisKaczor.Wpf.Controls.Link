fd57c19bd6502b65df874201af35dacfafcd2c0b
Chris Kaczor <chris@kaczorzoo.net>
2023-04-07 14:06:42 -0400

    Initial commit

---
+++ LinkControl.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+
+namespace ChrisKaczor.Wpf.Controls
+{
+    public partial class Link
+    {
+        public event RoutedEventHandler? Click;
+
+        public Link()
+        {
+            InitializeComponent();
+
+            HyperlinkControl.Click += HandleHyperlinkControlClick;
+        }
+
+        private void HandleHyperlinkControlClick(object sender, RoutedEventArgs e)
+        {
+            Click?.Invoke(sender, e);
+        }
+
+        public string Text
+        {
+            get => ContentControl.Text;
+            set => ContentControl.Text = value;
+        }
+
+        public new bool IsEnabled
+        {
+            get => base.IsEnabled;
+            set
+            {
+                base.IsEnabled = value;
+                HyperlinkControl.IsEnabled = value;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+﻿# ChrisKaczor.Wpf.Controls.Link
+
+This library can be used to restrict an application to only a single instance and sends the command line of any newly started instance to the currently running instance.
+
+## Authors
+
+* **Chris Kaczor** - *Initial work* - https://github.com/ckaczor - https://chriskaczor.com
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
+
+## Disclaimer
+
+This code is used by my various personal projects and may not be useful or work in all use cases.
